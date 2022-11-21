f4d2c9b902faeeab5c90c01394f02b45d1c8cdb7
zabs22 <zabsdere@gmail.com>
2022-11-21 11:15:52 +0300

    First Design

---
+++ reserve-web/src/pages/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
+
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header/>
+      <div className="homeContainer">
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Hotels guests love</h1>
+        <Footer/>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
